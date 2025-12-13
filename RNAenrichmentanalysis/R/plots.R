b6b77db2de90e939532bf1ed2065ca5ed1582ee3
vasilis <vasilis@heraclid.mynet>
2025-12-13 06:15:35 +0100

    For the plots it is basic plots nothing much I will modify it later, in the description another package was added

---
+++ RNAenrichmentanalysis/R/plots.R
@@ -0,0 +1,37 @@
+#'plots
+#'@description
+#'take the data from Over_representation (OR_data)
+#'
+#'
+#'
+#'@param OR_data a list of the GO data and KEGG data for the graphs ensure you use the $ to specify results of the enrichment
+#'@param categories is how many categories would be used for the plot
+#'
+#'@return returns an excel document with the DEG data
+#'
+#'@example
+#' Plots<- function(OR_data, )
+#'
+#'@import openxlsx enrichplot ggplot2
+#'@export
+#'
+
+#Plot function defining the inputs
+Plots<- function(OR_data, categories="20"){
+
+  #tree plot
+  OR_termsim<- pairwise_termsim(OR_data)
+  tree<- treeplot(OR_termsim, showCategory=categories)
+
+  #dot plot
+  dot<- dotplot(OR_data, title="plot of enriched data", showCategory=categories)
+
+  #cnet plot
+  netplot<- cnetplot(OR_data, showCategory=categories, foldChange=NULL)
+
+  #list of plots that will be returned
+  allplots<-list(tree_plot=tree, dot_plot=dot, net_plot<-netplot)
+
+  return(allplots)
+
+}
